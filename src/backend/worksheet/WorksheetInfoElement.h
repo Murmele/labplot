d45afcc53e9e847d50e1133f42c43b9fbe4a96fc
Unknown <martin.marmsoler@gmail.com>
2018-12-09 15:06:25 +0100

    Add ability to create multiple custom points on different curves with on marker

--- src/backend/worksheet/WorksheetInfoElement.h
+++ src/backend/worksheet/WorksheetInfoElement.h
@@ -16,14 +16,25 @@ class WorksheetInfoElement : public WorksheetElement
 {
     Q_OBJECT
 public:
+    WorksheetInfoElement(const QString& name, CartesianPlot *plot);
     WorksheetInfoElement(const QString& name, CartesianPlot *plot, const XYCurve* curve);
     void setParentGraphicsItem(QGraphicsItem* item);
 
     void save(QXmlStreamWriter*) const override;
     bool load(XmlStreamReader*, bool preview) override;
     void init();
+    void addCurve(XYCurve* curve, CustomPoint *custompoint= nullptr);
+    void addCurvePath(QString &curvePath, CustomPoint* custompoint = nullptr);
+    bool assignCurve(const QVector<XYCurve*> &curves);
+    void removeCurve(XYCurve* curve);
 
     QGraphicsItem* graphicsItem() const override;
+
+    struct MarkerPoints_T{
+        CustomPoint* customPoint;
+        const XYCurve* curve;
+        QString curvePath;
+    };
 
     bool isVisible() const override;
     void setVisible(bool on) override;
@@ -37,7 +48,9 @@ public slots:
 protected:
     WorksheetInfoElementPrivate* const d_ptr;
 private:
+    TextLabel* label;
     Q_DECLARE_PRIVATE(WorksheetInfoElement)
+    QVector<struct MarkerPoints_T> markerpoints;
 };
 
 #endif // WORKSHEETINFOELEMENT_H
